18e968d3b6be7fd04237f5577092a4b6e745ab72
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2020-03-31 17:40:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import BandsList from './components/BandsList';
+import Modal from './components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    bands: [],
+    currentBand: {}
+  };
+
+  changeCurrentBand = band => {
+    this.setState({
+      currentBand: band
+    });
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://raw.githubusercontent.com/Danilovesovic/bands/master/bands_with_id.json'
+    )
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          bands: data
+        })
+      );
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <BandsList
+          bands={this.state.bands}
+          changeCurrentBand={this.changeCurrentBand}
+        />
+        <Modal currentBand={this.state.currentBand} />
+      </div>
+    );
+  }
 }
 
 export default App;
