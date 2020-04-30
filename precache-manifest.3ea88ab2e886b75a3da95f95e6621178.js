35d63d164b93e935f485ca3704986bf7b5321396
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2020-04-30 19:59:09 +0200

    Updates

---
+++ precache-manifest.3ea88ab2e886b75a3da95f95e6621178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2879a055c0b03108ee45bcf4e6ccb696",
+    "url": "/react-bands-app/index.html"
+  },
+  {
+    "revision": "b20939e7946e68c4d012",
+    "url": "/react-bands-app/static/css/main.3acafc07.chunk.css"
+  },
+  {
+    "revision": "e6faf65a6963c7273a6a",
+    "url": "/react-bands-app/static/js/2.27ba4b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bands-app/static/js/2.27ba4b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20939e7946e68c4d012",
+    "url": "/react-bands-app/static/js/main.efc2c88e.chunk.js"
+  },
+  {
+    "revision": "af60bc955d638cb86a6d",
+    "url": "/react-bands-app/static/js/runtime-main.81a33090.js"
+  }
+]);
